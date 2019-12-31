e8444638c257d5c888c785dc292d6ca4659d7088
Archit Gupta <guptaarchit.ag@gmail.com>
2019-12-31 15:43:58 -0500

    Add all my files

--- src/pages/views.py
+++ src/pages/views.py
@@ -6,5 +6,18 @@ def home_view(request,*args,**kwargs):
 	print(request.user)
 	#return HttpResponse("<h1>Hello World</h1>")
 	return render (request, "home.html", {} )
-def contact_view(*args, **kwargs):
-	return HttpResponse("<h1>Contact View</h1>")
+
+def contact_view(request,*args, **kwargs):
+	return render(request, "contact.html",{})
+
+def about_view(request,*args,**kwargs):
+	my_context = {
+		"my_text": "This is about us",
+		"this_is_true":True,
+		"my_number": 123,
+		"my_list": [1313, 4231, 312, 'Abc']
+	}
+	return render(request, "about.html", my_context)
+
+def social_view(*args,**kwargs):
+	return HttpResponse("<h1>Social Page</h1>")
--- src/products/views.py
+++ src/products/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from .models import Product
+from .forms import ProductForm
 
 # Create your views here.
+# def product_create_view(request):
+# 	form = ProductForm(request.POST or None)
+# 	if form.is_valid():
+# 		form.save()
+# 	context ={
+# 		'form':form
+# 	}
+# 	return render(request, "products/product_create.html", context)
+
+def product_create_view(request):
+	# print(request.GET)
+	# print(request.POST)
+	my_new_title = request.POST.get('title')
+	print(my_new_title)
+	# Product.objects.create(title = my_new_title)
+	context ={
+	}
+	return render(request, "products/product_create.html", context)
+
+def product_detail_view(request):
+	obj = Product.objects.get(id=1)
+	# context = {
+	# 	'title': obj.title,
+	# 	'description': obj.description			
+	# }
+	context = {
+		'object': obj
+	}
+
+	return render(request, "products/product_detail.html", context)
